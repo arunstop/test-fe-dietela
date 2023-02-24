2ce3d2379b52e5971c908dfc6ab9ec9c77c3ecaa
Arunstop <arunstop@gmail.com>
2023-02-24 12:28:53 +0000

    chore : installed pinia and charka ui

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueCompositionApi from '@vue/composition-api'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+import Chakra, { CThemeProvider } from '@chakra-ui/vue'
 
 Vue.use(VueCompositionApi)
+Vue.use(PiniaVuePlugin)
+Vue.use(Chakra)
+const pinia = createPinia()
 
-new Vue(App).$mount('#app')
+new Vue({
+  el: `#app`,
+  pinia,
+  render: (h) => h(CThemeProvider, [h(App)]),
+})
